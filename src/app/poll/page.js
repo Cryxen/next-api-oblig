028e7ac8833c52fbafc6c436e7f361b32bf2c9d8
hakonstrom <hakon.s.strom@nmbu.no>
2023-10-20 17:35:56 +0200

    useState in poll works

--- src/app/poll/page.js
+++ src/app/poll/page.js
@@ -1,36 +1,73 @@
-"use client"
+"use client";
 
-import { useState } from "react"
+import { useState } from "react";
 
 const PollPage = () => {
-    const [isChecked, setIsChecked] = useState(false)
+  const [isChecked, setIsChecked] = useState([]);
 
-    const handleCheckBox = (event) => {
-        console.log(event.target.value)
+  // Function triggered when checking a checkbox
+  const handleCheckBox = (event) => {
+    console.log(event.target.value);
+    // Check if isChecked is empty
+    if(isChecked.length < 1){
+        console.log("if checked tom")
+        setIsChecked([event.target.value])
+        console.log(isChecked)
     }
-
-    const checkbox1 = "checkbox1"
-    const checkbox2 = "checkbox2"
-    const checkbox3 = "checkbox3"
-
-    const formSubmit = (event) => {
-        console.log("Form submitted")
-        event.preventDefault()
+    else {
+        // There is something in "isChecked". See if event target value exists in isChecked. Add if not, if it is - delete.
+        if (isChecked.indexOf(event.target.value) <= -1) {
+            console.log("Checkbox er ikke i array")
+            setIsChecked([...isChecked, event.target.value])
+        }
+        else {
+            console.log("Checkbox er i array")
+            const index = isChecked.indexOf(event.target.value)
+            setIsChecked(isChecked.filter(item => item !== event.target.value))
+        }
     }
-    return (
-        <div className="pollPage">
-            <h1>Pollpage</h1>
-            <form onSubmit={formSubmit}>
-                <p>Hva synes du om dårlgi mat?</p>
-                <label htmlFor={checkbox1} >{checkbox1}</label>
-                <input type="checkbox" name={checkbox1} value={checkbox1} onChange={handleCheckBox}/>
-                <label htmlFor={checkbox2}>{checkbox2}</label>
-                <input type="checkbox" name={checkbox2} value={checkbox2} onChange={handleCheckBox}/>
-                <label htmlFor={checkbox3}>{checkbox3}</label>
-                <input type="checkbox" name={checkbox3} value={checkbox3} onChange={handleCheckBox}/>
-                <button type="submit">Submit poll</button>
-            </form>
-        </div>
-    )
 }
-export default PollPage
+
+  console.log(isChecked)
+
+  const checkbox1 = "checkbox1";
+  const checkbox2 = "checkbox2";
+  const checkbox3 = "checkbox3";
+
+  // Button handler for submitting form
+  const formSubmit = (event) => {
+    console.log("Form submitted");
+    event.preventDefault();
+  };
+  return (
+    <div className="pollPage">
+      <h1>Pollpage</h1>
+      <form onSubmit={formSubmit}>
+        <p>Hva synes du om dårlgi mat?</p>
+        <label htmlFor={checkbox1}>{checkbox1}</label>
+        <input
+          type="checkbox"
+          name={checkbox1}
+          value={checkbox1}
+          onChange={handleCheckBox}
+        />
+        <label htmlFor={checkbox2}>{checkbox2}</label>
+        <input
+          type="checkbox"
+          name={checkbox2}
+          value={checkbox2}
+          onChange={handleCheckBox}
+        />
+        <label htmlFor={checkbox3}>{checkbox3}</label>
+        <input
+          type="checkbox"
+          name={checkbox3}
+          value={checkbox3}
+          onChange={handleCheckBox}
+        />
+        <button type="submit">Submit poll</button>
+      </form>
+    </div>
+  );
+};
+export default PollPage;
