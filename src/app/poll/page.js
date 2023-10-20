ec81f603241a45c8bf6f9877b2edd5e937f79276
hakonstrom <hakon.s.strom@nmbu.no>
2023-10-20 17:55:04 +0200

    connected polls to API

--- src/app/poll/page.js
+++ src/app/poll/page.js
@@ -1,34 +1,48 @@
 "use client";
 
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const PollPage = () => {
   const [isChecked, setIsChecked] = useState([]);
+  const [polls, setPolls] = useState([{}]);
 
+  // Fetch polls from API
+  useEffect(() => {
+    const fetchPollsFromAPI = async () => {
+      const response = await axios("/api/polls");
+      if (response.status === 200) {
+        setPolls(response.data.data)
+      }
+      else
+        console.log("Something went wrong")
+    };
+    fetchPollsFromAPI();
+  }, []);
+
+  console.log(polls)
   // Function triggered when checking a checkbox
   const handleCheckBox = (event) => {
     console.log(event.target.value);
     // Check if isChecked is empty
-    if(isChecked.length < 1){
-        console.log("if checked tom")
-        setIsChecked([event.target.value])
-        console.log(isChecked)
+    if (isChecked.length < 1) {
+      console.log("if checked tom");
+      setIsChecked([event.target.value]);
+      console.log(isChecked);
+    } else {
+      // There is something in "isChecked". See if event target value exists in isChecked. Add if not, if it is - delete.
+      if (isChecked.indexOf(event.target.value) <= -1) {
+        console.log("Checkbox er ikke i array");
+        setIsChecked([...isChecked, event.target.value]);
+      } else {
+        console.log("Checkbox er i array");
+        const index = isChecked.indexOf(event.target.value);
+        setIsChecked(isChecked.filter((item) => item !== event.target.value));
+      }
     }
-    else {
-        // There is something in "isChecked". See if event target value exists in isChecked. Add if not, if it is - delete.
-        if (isChecked.indexOf(event.target.value) <= -1) {
-            console.log("Checkbox er ikke i array")
-            setIsChecked([...isChecked, event.target.value])
-        }
-        else {
-            console.log("Checkbox er i array")
-            const index = isChecked.indexOf(event.target.value)
-            setIsChecked(isChecked.filter(item => item !== event.target.value))
-        }
-    }
-}
+  };
 
-  console.log(isChecked)
+  console.log(isChecked);
 
   const checkbox1 = "checkbox1";
   const checkbox2 = "checkbox2";
