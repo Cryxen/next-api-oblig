efea2bcd875ddd7993feedc900dab8a37de02afd
hakonstrom <hakon.s.strom@nmbu.no>
2023-10-21 17:59:30 +0200

    made post API on polls

--- src/app/api/polls/route.js
+++ src/app/api/polls/route.js
@@ -4,3 +4,11 @@ import { NextResponse } from "next/server"
 export const GET = () => {
     return NextResponse.json({data: Polls, status: 200})
 }
+
+export const POST = async(req) => {
+    console.log("POST FORSØK")
+    const data = await req.json()
+    console.log(data)
+    console.log(Polls.push(data))
+    return NextResponse.json(data,{ status: 201 });
+}
