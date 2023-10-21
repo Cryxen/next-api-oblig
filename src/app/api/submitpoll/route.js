04fa4860941e3106a59b94fcfd64a2576ed4d5c1
hakonstrom <hakon.s.strom@nmbu.no>
2023-10-21 18:50:24 +0200

    created submit poll api

---
+++ src/app/api/submitpoll/route.js
@@ -0,0 +1,15 @@
+import { PollDelivered } from "@/features/polls/Polls"
+import { NextResponse } from "next/server"
+
+export const GET = () => {
+    return NextResponse.json({data: PollDelivered, status: 200})
+
+}
+
+export const POST = async(req) => {
+    console.log("POST FORSØK")
+    const data = await req.json()
+    console.log(data)
+    console.log(PollDelivered.push(data))
+    return NextResponse.json(data,{ status: 201 });
+}
