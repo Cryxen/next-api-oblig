8c3eb579e46b806033ee3f9956bc4ad1e1155cfd
hakonstrom <hakon.s.strom@nmbu.no>
2023-10-21 14:21:10 +0200

    Made dumb createpoll page

--- src/app/createpoll/page.js
+++ src/app/createpoll/page.js
@@ -1,10 +1,25 @@
+"use client"
 import Layout from "../components/Layout"
 
 const CreatePoll = () => {
 
+    const numberOfQuestions = 2
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log("Submit button pressed")
+    }
+
     return(
         <Layout>
             <h1>Lag spørreundersøkelse!</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="pollTitle">Navn på spørreundersøkelse</label>
+                <input type="text" name="pollTitle" />
+                <label htmlFor="pollQuestion">Spørsmål</label>
+                <input type="text" name="pollQuestion" />
+                <button type="submit">Lag undersøkelse</button>
+            </form>
         </Layout>
     )
 }
