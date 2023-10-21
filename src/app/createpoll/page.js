38e52e724bd6c602fd0c6cb964f837724e7fd2e0
hakonstrom <hakon.s.strom@nmbu.no>
2023-10-21 18:02:58 +0200

    Made logic in create poll and connected to api

--- src/app/createpoll/page.js
+++ src/app/createpoll/page.js
@@ -1,13 +1,45 @@
 "use client"
+import { useState } from "react"
 import Layout from "../components/Layout"
+import axios from "axios"
 
 const CreatePoll = () => {
-
+    const [pollTitle, setPollTitle] = useState('')
+    const [pollQuestion1, setPollQuestion1] = useState('')
+    const [pollQuestion2, setPollQuestion2] = useState('')
+    const [pollQuestion3, setPollQuestion3] = useState('')
+    
     const numberOfQuestions = 2
+
 
     const handleSubmit = (event) => {
         event.preventDefault()
         console.log("Submit button pressed")
+        const poll = {
+            question: pollTitle,
+            checkbox1: pollQuestion1,
+            checkbox2: pollQuestion2,
+            checkbox3: pollQuestion3
+        }
+        axios.post('/api/polls', poll)
+            .then((response) => {
+                console.log(response)
+            })
+            .catch((error) => {console.log(error)})
+    }
+
+    const handlePollTitleChange = (event) => {
+        setPollTitle(event.target.value)
+    }
+
+    const handlePollQuestion1 = (event) => {
+        setPollQuestion1(event.target.value)
+    }
+    const handlePollQuestion2 = (event) => {
+        setPollQuestion2(event.target.value)
+    }
+    const handlePollQuestion3 = (event) => {
+        setPollQuestion3(event.target.value)
     }
 
     return(
@@ -15,9 +47,13 @@ const CreatePoll = () => {
             <h1>Lag spørreundersøkelse!</h1>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="pollTitle">Navn på spørreundersøkelse</label>
-                <input type="text" name="pollTitle" />
-                <label htmlFor="pollQuestion">Spørsmål</label>
-                <input type="text" name="pollQuestion" />
+                <input type="text" name="pollTitle" onChange={handlePollTitleChange}/>
+                <label htmlFor="pollQuestion1">Svar alternativ 1</label>
+                <input type="text" name="pollQuestion1" onChange={handlePollQuestion1}/>
+                <label htmlFor="pollQuestion2">Svar alternativ 2</label>
+                <input type="text" name="pollQuestion2" onChange={handlePollQuestion2}/>
+                <label htmlFor="pollQuestion3">Svar alternativ 3</label>
+                <input type="text" name="pollQuestion3" onChange={handlePollQuestion3}/>
                 <button type="submit">Lag undersøkelse</button>
             </form>
         </Layout>
