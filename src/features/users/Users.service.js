92ab5e1026bb82e0f23ce172b29fc49f104e22db
hakonstrom <hakon.s.strom@nmbu.no>
2023-10-28 01:50:24 +0200

    made a check to see if user existed

--- src/features/users/Users.service.js
+++ src/features/users/Users.service.js
@@ -3,30 +3,46 @@ import * as usersRepo from "./Users.repository";
 import { NextResponse } from "next/server";
 
 
+const checkIfUserExists = async (userEmail) => {
+  const usersFromDb = await fetchAllUsers();
+  let matchedUser = null
+  usersFromDb.data.forEach((element) => {
+    if (element.email == userEmail) {
+      matchedUser = element
+    }
+  });
+  return matchedUser
+};
+
 export const addUser = async (userName, userEmail) => {
+  if (!validate.isEmail(userEmail)) {
+    console.log(userName);
+    return { success: false, error: "email check failed" };
+  }
 
+  if (!validate.isName(userName)) {
+    return { success: false, error: "name check failed. Please use full name" };
+  }
   
-  if (!validate.isEmail(userEmail)){
-    console.log(userName)
-    return {success: false, error: "email check failed"}
+  const userCheck = await checkIfUserExists(userEmail);
+  if(userCheck){
+    console.log("User found!")
+    return {success: true, data: userCheck}
   }
 
-  if (!validate.isName(userName)){
-    return {success: false, error: "name check failed. Please use full name"}
-  }
   const addedUserToList = await usersRepo.addUser(userName, userEmail);
-  console.log(addedUserToList)
+
+
   if (addedUserToList.success) {
     return { success: true, data: addedUserToList.data };
   } else return { success: false, error: addedUserToList.error };
 };
 
-
 export const fetchAllUsers = async () => {
   try {
-    const usersFromDb = await usersRepo.fetchAllUsers()
-    return{success: true, data: usersFromDb.data}
+    const usersFromDb = await usersRepo.fetchAllUsers();
+    return { success: true, data: usersFromDb.data };
   } catch (error) {
-    return {success: false, data: usersFromDb.error}
+    return { success: false, data: usersFromDb.error };
   }
-}
+};
